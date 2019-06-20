f956e08e9dbfa822caa8b33fe6954852927b5beb
admin <admin@admins-MacBook-Air.local>
2019-06-20 17:29:28 +0700

    remove dataVersion column

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use('/voyager', voyagerMiddleware({ endpointUrl: '/graphql' }));
+  // su dung global pipe
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
   Logger.log(`🚀 Server running on http://localhost:${port}`, 'Bootstrap');
--- src/users/user.input.ts
+++ src/users/user.input.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, Length, IsEmail } from 'class-validator';
 export class UserInput {
   @IsNotEmpty()
   @Length(10, 20)
-  // @IsEmail()
+  @IsEmail()
   username: string;
 
   @IsNotEmpty()
