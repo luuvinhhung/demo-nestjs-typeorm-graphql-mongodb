8ddecd431f5afe42bb5891352fb3dfaad2754110
admin <admin@admins-MacBook-Air.local>
2019-06-21 18:01:37 +0700

    remove dataversion column in entity

--- src/users/user.input.ts
+++ src/users/user.input.ts
@@ -2,7 +2,7 @@ import { IsNotEmpty, Length, IsEmail } from 'class-validator';
 
 export class UserInput {
   @IsNotEmpty()
-  @Length(10, 20, { message: 'email must be at least 10 charatcers' })
+  @Length(10, 20, { message: 'email must be at least 10 characters' })
   @IsEmail()
   email: string;
 
